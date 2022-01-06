9b61469c1b84d4e3fb795e7360c3b5b6a7109734
Вячеслав Илюхин <iluxin-slava@yandex.ru>
2022-01-06 07:45:32 +0300

    Добавлен базовый класс команды

---
+++ SecurityGuard/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using SecurityGuard.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel 
+    {
+        #region Заголовок окна
+        
+        private string _Title = "Охранник";
+        
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title 
+        {
+            get => _Title;
+
+            set => Set(ref _Title, value); //3
+            // возможные варианты написание set
+            //set
+            //{
+            //    1//if (Equals(_Title, value)) return;
+            //    1//_Title = value;                          
+            //    1//OnPropertyChanged();
+
+            //    2//Set(ref _Title, value);
+            //}
+        }
+        #endregion
+    }
+}
